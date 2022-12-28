84ee7ba95def836ad359d8352b5c03200aeec589
Zixuan Zhang <zixuanzhang@PikachuStation>
2022-12-28 18:28:12 -0500

    Add rpn-calculator

---
+++ rust/rpn-calculator/src/lib.rs
@@ -0,0 +1,44 @@
+#[derive(Debug)]
+pub enum CalculatorInput {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Value(i32),
+}
+
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    if inputs.len() == 0 {
+        return None;
+    }
+
+    let mut stack = Vec::new();
+
+    for input in inputs {
+        match input {
+            CalculatorInput::Value(x) => stack.push(*x),
+            _ => {
+                if stack.len() < 2 {
+                    return None;
+                }
+
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+
+                match input {
+                    CalculatorInput::Add => stack.push(a + b),
+                    CalculatorInput::Subtract => stack.push(a - b),
+                    CalculatorInput::Divide => stack.push(a / b),
+                    CalculatorInput::Multiply => stack.push(a * b),
+                    _ => return None,
+                }
+            }
+        }
+    }
+
+    if stack.len() != 1 {
+        None
+    } else {
+        stack.pop()
+    }
+}
