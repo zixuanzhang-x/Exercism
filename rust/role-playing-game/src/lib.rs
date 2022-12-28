e3a7c81bed2498e0c46f1844d7425f4e4a1f3bc4
Zixuan Zhang <zixuanzhang@PikachuStation>
2022-12-28 14:48:02 -0500

    Add role-playing-game

---
+++ rust/role-playing-game/src/lib.rs
@@ -0,0 +1,51 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        if self.health == 0 {
+            match self.level {
+                10.. => Some(Player {
+                    health: 100,
+                    mana: Some(100),
+                    level: self.level,
+                }),
+                _ => Some(Player {
+                    health: 100,
+                    mana: None,
+                    level: self.level,
+                }),
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        match self.mana {
+            Some(amount) => {
+                if amount >= mana_cost {
+                    self.mana = Some(amount - mana_cost);
+                    2 * mana_cost
+                } else {
+                    0
+                }
+            }
+            None => {
+                self.health = if self.health > mana_cost {
+                    self.health - mana_cost
+                } else {
+                    0
+                };
+                0
+            }
+        }
+    }
+}
